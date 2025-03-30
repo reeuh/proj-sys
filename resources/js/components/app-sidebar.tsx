6e7255e22445f6c532962c8d59cd29e109f5a5af
Faye Lacsi <113992925+reeuh@users.noreply.github.com>
2025-03-31 02:30:33 +0800

    fixes

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -4,32 +4,39 @@ import { NavUser } from '@/components/nav-user';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/react';
-import { BookOpen, Folder, LayoutGrid } from 'lucide-react';
+import { BookOpen, Folder, LayoutGrid, Archive } from 'lucide-react';
 import AppLogo from './app-logo';
 
 const mainNavItems: NavItem[] = [
     {
-        title: 'Dashboard',
+        title: 'Home',
         href: '/dashboard',
         icon: LayoutGrid,
     },
+    {
+        title: 'Capstone',
+        href: '#',
+        icon: Folder,
+    },
+    {
+        title: 'Grades',
+        href: '#',
+        icon: BookOpen,
+    },
+
 ];
 
 const footerNavItems: NavItem[] = [
     {
-        title: 'Repository',
-        href: 'https://github.com/laravel/react-starter-kit',
-        icon: Folder,
-    },
-    {
-        title: 'Documentation',
-        href: 'https://laravel.com/docs/starter-kits',
-        icon: BookOpen,
+        title: 'Archive',
+        href: '#',
+        icon: Archive,
     },
 ];
 
 export function AppSidebar() {
     return (
+
         <Sidebar collapsible="icon" variant="inset">
             <SidebarHeader>
                 <SidebarMenu>
--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -11,20 +11,20 @@ export default function Welcome() {
                 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
             </Head>
             <div className="bg-end flex min-h-screen flex-col items-center bg-[url(images/cics1.png)] bg-[200px] bg-no-repeat p-6 lg:justify-center lg:p-8 bg-[#85B1C3]">
-                <header className="fixed top-0 left-0 z-50 flex w-full justify-between border-b border-black bg-[#4076B4] p-5">
+                <header className="fixed top-0 left-0 z-50 flex w-full justify-between bg-[#4076B4] p-1">
                     <div className="mx-auto w-full px-4 sm:px-6 lg:px-8 xl:px-12">
                         <div className="flex h-16 items-center justify-between lg:h-[72px]">
                             <div className="hidden lg:flex lg:justify-center lg:space-x-10 xl:space-x-14"></div>
                             <div className="flex items-center justify-end space-x-5">
                                 <Link
                                     href={route('login')}
-                                    className="inline-block rounded-sm border border-transparent px-5 py-1.5 text-2xl text-white bg-[#4076B4] hover:bg-[#305a8a] hover:border-white transition duration-200"
+                                    className="inline-block rounded-sm border border-t-zinc-800 px-3 py-1 text-xl text-white bg-[#4076B4] hover:bg-[#305a8a] hover:border-white transition duration-200"
                                 >
                                     Log in
                                 </Link>
                                 <Link
                                     href={route('register')}
-                                    className="inline-block rounded-sm border border-transparent px-5 py-1.5 text-2xl text-white bg-[#4076B4] hover:bg-[#305a8a] hover:border-white transition duration-200"
+                                    className="inline-block rounded-sm border border-t-zinc-800 px-3 py-1 text-xl text-white bg-[#4076B4] hover:bg-[#305a8a] hover:border-white transition duration-200"
                                 >
                                     Register
                                 </Link>
@@ -44,29 +44,15 @@ export default function Welcome() {
                             </p>
                         </div>
                     </div>
-
-                    <div className="mt-8 lg:hidden">
-                        <img
-                            className="h-full w-full object-cover"
-                            src="https://cdn.rareblocks.xyz/collection/clarity-ecommerce/images/hero/1/bg.png"
-                            alt=""
-                        />
-                    </div>
                     <nav className="flex items-center justify-end gap-4">
                         {auth.user ? (
-                            <Link
-                                href={route('dashboard')}
-                                className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
-                            >
-                                Dashboard
-                            </Link>
-                        ) : (
+                
                             <>
                                 <div className="flex w-full items-center justify-end">
                                     <div className="flex w-md items-center justify-center gap-5"></div>
                                 </div>
                             </>
-                        )}
+                        ) : null}
                     </nav>
                 </header>
                 <div className="hidden h-14.5 lg:block"></div>
